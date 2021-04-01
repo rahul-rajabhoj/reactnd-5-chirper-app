9660dae12849aadbeec981fdf5d0999cc7d30877
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-01 12:17:55 +0530

    Added saving of parent tweet to tweet object

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,11 @@ import { formatTweet } from '../utils/helpers'
  
 class Tweet extends React.Component {
     render() {
+        const { tweet } = this.props
+
+        if(!tweet) {
+            return <p>This tweet doesn't exists</p>
+        }
         console.log(this.props)
         return(
             <div className='tweet'>
@@ -15,9 +20,14 @@ class Tweet extends React.Component {
 
 function mapStateToProps({ authedUser, users, tweets}, {id}) {
     const tweet = tweets[id];
+    const parentTweet = tweet 
+        ? tweets[tweet.replyingTo] 
+        : null
     return { 
         authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
+        tweet: tweet 
+            ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+            : null
     }
 }
 
