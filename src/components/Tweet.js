5012f13e96be12d804e098d6f8232465490c84c6
Rahul Rajabhoj <rahulrajabhoj@gmail.com>
2021-04-01 23:06:29 +0530

    Added dispatch of toggle_tweet action on liking tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,6 +4,7 @@ import { formatTweet, formatDate } from '../utils/helpers'
 import TiArrowBackOutline from 'react-icons/lib/ti/arrow-back-outline'
 import TiHeartOutline from 'react-icons/lib/ti/heart-outline'
 import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline'
+import { handleToggleTweet } from '../actions/tweets'
 
 
 class Tweet extends React.Component {
@@ -13,9 +14,14 @@ class Tweet extends React.Component {
     }
 
     handleLike = (e) => {
+        const { tweet, dispatch, authedUser } = this.props
         e.preventDefault()
 
-        // todo: handle like
+        dispatch(handleToggleTweet({
+            id: tweet.id,
+            hasLiked: tweet.hasLiked,
+            authedUser,
+        }));
     }
 
     render() {
